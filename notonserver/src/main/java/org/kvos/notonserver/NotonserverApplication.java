294ad2763c5a7e86e89cfa9190dd7a1b6938cb31
Korjan Vos <korjanvos@gmail.com>
2019-05-17 22:09:21 +0200

    Initial commit

---
+++ notonserver/src/main/java/org/kvos/notonserver/NotonserverApplication.java
@@ -0,0 +1,11 @@
+package org.kvos.notonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotonserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotonserverApplication.class, args);
+    }
+}
