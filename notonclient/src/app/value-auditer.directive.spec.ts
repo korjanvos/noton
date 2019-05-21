e2bf9928c1ea387c62d7dbb397762c23f8edf72c
Korjan Vos <korjanvos@gmail.com>
2019-05-21 21:50:25 +0200

    Fix mistake in testcase of value-auditer.directive.

--- notonclient/src/app/value-auditer.directive.spec.ts
+++ notonclient/src/app/value-auditer.directive.spec.ts
@@ -1,8 +1,10 @@
 import { ValueAuditerDirective } from './value-auditer.directive';
+import { ElementRef } from "@angular/core";
+import anything = jasmine.anything;
 
 describe('ValueAuditerDirective', () => {
   it('should create an instance', () => {
-    const directive = new ValueAuditerDirective();
+    const directive = new ValueAuditerDirective(new ElementRef(anything));
     expect(directive).toBeTruthy();
   });
 });
